3520c37a0c6908899ba6a7ff4ab5e7589d733fb6
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-06-30 17:07:48 -0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,31 @@
+## This is a script to import .tex files formatted using the tb-to-tex library (https://github.com/rebeccaeverson/tb-to-tex/) into an .xlsx file. 
+
+import re 
+from import openpyxl import Workbook
+
+columns = [“LX”,”HM”, “PH”, “PS”, “GE”, “XV”, “XE”, “SG”, “OP”, “TP”, “SE”, “VA”]
+
+workbook = Workbook()
+script = workbook.active
+script.append(columns) 
+
+with open(“xyz.tex”, r) as file:
+	text = file.read()
+	entries = text.split(“\n”)
+      for entry in entries:
+		if entry.startswith(“\\tbLX”):
+			if row in locals():
+				script.append(row)
+		row = [""]*len(columns)
+
+		for ctg, col in enumerate(columns):
+			entered_text = “\\tb” + col + “”
+			doesMatch = re.search(entered_text, entry)
+			if doesMatch is not None: 
+				row[ctg] = match.group(1)
+
+
+if row in locals():
+script.append(row) ## if row variable exists, means that we’ve collected that row. This handles the last row.
+
+workbook.save(“output.xlsx”)

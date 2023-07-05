4cd27772105801d0f4846e058015a6761dbc8c5f
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-07-05 11:32:59 -0700

    Update script.py

--- script.py
+++ script.py
@@ -10,31 +10,35 @@ with open('d.tex', 'r') as file:
     lines = file.readlines()
     for line in lines:
         line = line.strip()
-        if line.startswith('%') or line == '':
-            continue
+        if not line.startswith('\tbLX'):
+            continue             # we want to skip over lines that start with %, as well as lines that are empty (that start with '') 
         lx = re.search(r'\\tbLX\{(.*?)\}', line)
         hm = re.search(r'\\tbHM\{(.*?)\}', line)
         ph = re.search(r'\\tbPH\{(.*?)\}', line)
         ps = re.search(r'\\tbPS\{(.*?)\}', line)
         ge = re.search(r'\\tbGE\{(.*?)\}', line)
+        xv = re.search(r'\\tbXV\{(.*?)\}', line)
+        xe = re.search(r'\\tbXE\{(.*?)\}', line)
+        sg = re.search(r'\\tbSG\{(.*?)\}', line)
         op = re.search(r'\\tbOP\{(.*?)\}', line)
         tp = re.search(r'\\tbTP\{(.*?)\}', line)
-        pd = re.search(r'\\tbPD\{(.*?)\}', line)
         va = re.search(r'\\tbVA\{(.*?)\}', line)
-        sn = re.search(r'\\tbSN \\\tbGE\{(.*?)\}', line)
+        se = re.search(r'\\tbSN \\\tbGE\{(.*?)\}', line)
 
         lx = lx.group(1) if lx is not None else ''
         hm = hm.group(1) if hm is not None else ''
         ph = ph.group(1) if ph is not None else ''
         ps = ps.group(1) if ps is not None else ''
         ge = ge.group(1) if ge is not None else ''
+        xv = xv.group(1) if xv is not None else ''
+        xe = xe.group(1) if xe is not None else ''
+        sg = sg.group(1) if sg is not None else ''
         op = op.group(1) if op is not None else ''
         tp = tp.group(1) if tp is not None else ''
-        pd = pd.group(1) if pd is not None else ''
         va = va.group(1) if va is not None else ''
-        sn = sn.group(1) if sn is not None else ''
-        
-        data.append([lx, hm, ph, ps, ge, op, tp, pd, va, sn])
+        se = se.group(1) if se is not None else ''
+                
+        data.append([lx, hm, ph, ps, ge, xv, xe, sg, op, tp, va, se])
 
-df = pd.DataFrame(data, columns=['LX', 'HM', 'PH', 'PS', 'GE', 'OP', 'TP', 'PD', 'VA', 'SN'])
+df = pd.DataFrame(data, columns=['LX', 'HM', 'PH', 'PS', 'GE', 'SV', 'XE', 'SG', 'OP', 'TP', 'VA', 'SE'])
 df.to_excel('d.xlsx', index=False)

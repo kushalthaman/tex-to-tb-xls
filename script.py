3d283716d0dd2f847e9607814f95f0c51d69984b
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-07-05 11:12:36 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,29 +1,38 @@
 ## This is a script to import .tex files formatted using the tb-to-tex library (https://github.com/rebeccaeverson/tb-to-tex/) into an .xlsx file. 
 
-import re 
-from openpyxl import Workbook
+import re
 import pandas as pd
 
-columns = ["LX","HM", "PH", "PS", "GE", "XV", "XE", "SG", "OP", "TP", "SE", "VA"]
+data = []
+with open('d.tex', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        line = line.strip()
+        if line.startswith('%') or line == '':
+            continue
+        lx = re.search(r'\\tbLX\{(.*?)\}', line)
+        hm = re.search(r'\\tbHM\{(.*?)\}', line)
+        ph = re.search(r'\\tbPH\{(.*?)\}', line)
+        ps = re.search(r'\\tbPS\{(.*?)\}', line)
+        ge = re.search(r'\\tbGE\{(.*?)\}', line)
+        op = re.search(r'\\tbOP\{(.*?)\}', line)
+        tp = re.search(r'\\tbTP\{(.*?)\}', line)
+        pd = re.search(r'\\tbPD\{(.*?)\}', line)
+        va = re.search(r'\\tbVA\{(.*?)\}', line)
+        sn = re.search(r'\\tbSN \\\tbGE\{(.*?)\}', line)
 
-def process_tex_to_excel(tex_filepath, xlsx_filepath):
-    with open(tex_filepath, 'r', encoding='utf-8') as file:
-        data = file.read().replace('\n', ' ')
+        lx = lx.group(1) if lx is not None else ''
+        hm = hm.group(1) if hm is not None else ''
+        ph = ph.group(1) if ph is not None else ''
+        ps = ps.group(1) if ps is not None else ''
+        ge = ge.group(1) if ge is not None else ''
+        op = op.group(1) if op is not None else ''
+        tp = tp.group(1) if tp is not None else ''
+        pd = pd.group(1) if pd is not None else ''
+        va = va.group(1) if va is not None else ''
+        sn = sn.group(1) if sn is not None else ''
+        
+        data.append([lx, hm, ph, ps, ge, op, tp, pd, va, sn])
 
-    dictionary = {}
-
-    pattern = re.compile(r'\\tb([a-zA-Z]+)\{(.+?)\}')
-    matches = re.findall(pattern, data)
-
-    for match in matches:
-        category, value = match
-        if category == "LX":
-            current_lexeme = value
-            dictionary[current_lexeme] = {}
-        elif current_lexeme:
-            dictionary[current_lexeme][category] = value
-            
-    df = pd.DataFrame(dictionary).T
-    df.to_excel(xlsx_filepath)
-
-process_tex_to_excel('d.tex', 'd.xlsx')
+df = pd.DataFrame(data, columns=['LX', 'HM', 'PH', 'PS', 'GE', 'OP', 'TP', 'PD', 'VA', 'SN'])
+df.to_excel('d.xlsx', index=False)

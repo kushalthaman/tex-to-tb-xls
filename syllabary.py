831003f655697bf7c95238dbca0ac0c7b297a1fd
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-07-14 10:29:23 -0700

    Create syllabary.py

---
+++ syllabary.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def make_syllabary(df, column = "PH+Syl"):
+    syllables = []
+    for word in df[column].tolist():
+        syllables.extend(word.split('.'))
+    
+    syllabary = pd.DataFrame(syllables, columns=["syllable"])
+    syllabary['frequency'] = syllabary.groupby('syllable')['syllable'].transform('count')
+    return syllabary
+
+df = pd.read_excel("DagaareDict.xlsx")
+syllabary = make_syllabary(df)
+syllabary.to_excel("DagaareSyllabary.xlsx", index=False)
+
+  

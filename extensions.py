7bac4fd714602081c3d4160758792589bcbe9ece
Emiyare Ikwut-Ukwa <138727724+e-ikwut@users.noreply.github.com>
2023-07-19 23:31:24 -0700

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,33 +1,8 @@
-## Creates vowel, ATR, excluded, PH + Syl, Syl Count, Nasality, Tone, Tone + Syl
-
 import pandas as pd
 
-#cons_list = ["h", "n", "ŋ", "b", "r", "d", "k", "s", "t", "f", "g", "z", "m", "l", "p", "w", "y", "ʧ", "ɲ", "v", "g", "ʣ"]
-
-all_cons = "hnŋbrdkstfgzmlpwyʧɲvgʣ"
 all_vowels = "aáàâãạåeéèêɛiíìîɪoóòôõɔuúùûʊ"
-# extra_chars = "- "
-exclude_list = "\\Dạå᷈"
-digraphs = ["gb", "kp", "ŋm", "dz"]
-pATR_list = "eiou"
-nATR_list = "aɛɪɔʊ"
+all_cons = "hnŋbrdkstfgzmlpwyʧɲvgʣ"
 dipthongs = ["ie", "uo", "ɪɛ", "ʊɔ"]
-tones = {"á":"H", "é":"H", "í":"H", "ó":"H", "ú" : "H", "à":"L", "è":"L", "ì":"L", "ò":"L", "ù":"L", "â":"HL", "ê":"HL", "ô" : "HL", "û": "HL", "́":"H", "̀":"L", "̂":"HL", "̌":"LH"}
-nasal_chars = ["̃", "ã"]
-
-def first_seg(ind, word):
-    if ind+1 < len(word) and word[ind] + word[ind+1] in digraphs:
-        ind += 1
-    for char in word[ind+1:]:
-        if char in all_cons + all_vowels:
-            return char
-    return False
-
-def is_exclude(word):
-    for char in word:
-        if char in exclude_list:
-            return True
-    return False
 
 def accent_strip(c):
     if c == "á" or c == "à" or c == "â" or c == "ã" or c == "ạ" or c == "å":  # ạ å very strange
@@ -40,143 +15,81 @@ def accent_strip(c):
         return "o"
     if c == "ú" or c == "ù" or c == "û":
         return "u"
-
+    
     return c
-
-def syllabify(word):  # not sensible for excluded words
-    result = ""
-    prev_seg = None
-
-    for ind, char in enumerate(word):
-        if not (prev_seg == None or prev_seg + char in digraphs):
-            prev_stripped = accent_strip(prev_seg)
-            cur_stripped = accent_strip(char)
-            if (prev_seg in all_vowels and char in all_vowels) and (prev_stripped != cur_stripped and prev_stripped + cur_stripped not in dipthongs):
-                result += "."
-            elif prev_seg and prev_seg in all_cons and char in all_cons:
-                result += "."
-            elif prev_seg and prev_seg in all_vowels and char in all_cons and first_seg(ind, word) and first_seg(ind, word) in all_vowels:
-                result += "."
-
-        result += char
-
-        if char in all_cons or char in all_vowels:
-            prev_seg = char
-
-    return result
-
-def count_syl(word):
-    return word.count(".") + 1
 
 def vowelize(word):
     vowels = [accent_strip(char) for char in word if char.lower() in all_vowels or char == "."]
     return "".join(vowels)
 
-# nasal_chars = ["̃", "ã"]
-def nasalize(word):
+def templatize(syl):
     result = ""
-    for char in word:
-        if char == "̃":
-            result = result [:-1] + "N"
-        elif char in all_vowels:
-            result += "O"
-        elif char == ".":
-            result += "."
-    return result
+    for char in syl:
+        if char in all_vowels:
+            result += "V"
+        elif char in all_cons:
+            result += "C"
+    return result.replace("CC", "C")
 
-def tonalize(syllabified_word, do_syllabify):
-    tonalized_word = ""
-    last_tone = None
 
-    for char in syllabified_word:
-        if char == ".":
-            tonalized_word += char
-            last_tone = None  
-        elif char in tones and last_tone != tones[char]:
-            tonalized_word += tones[char]
-            last_tone = tones[char]
+def make_syllabary(df, column):
+    syllables = []
+    vowels_list = ["a","á","à","â","ã","ạ","å","a","e","é","è","ê","ɛ","i","í","ì","î","ɪ","o","ó","ò","ô","õ","ɔ","u","ú","ù","û","ʊ"]
+    consonants_list = list(all_cons)
 
-    if not do_syllabify:
-        tonalized_word = tonalized_word.replace(".", "")
-        
-    return tonalized_word
- 
-"""
-def tone_and_vowelize(word, do_syllabify):
-    word = syllabify(word) if do_syllabify else word
-    vowels = vowelize(word)
-    tone_word = tone(vowels)
-    return tone_word
-"""
-"""    
-def syllabify_nasal_and_tone(word, do_syllabify):
-    word = syllabify(word) if do_syllabify else word
-    nasal_word = nasalization(word)
-    tone_word = tone(word)
-    return nasal_word, tone_word
-"""
+    column_entries = df[column].astype(str)
+
+    for word in column_entries.tolist():
+        word_syls = word.split(".")
+        syl_1 = [word_syls[0].replace("-", ""), True] # removed dashes, must remember to document that we did this
+        syllables.append(syl_1)
+        word_syls.pop(0)
+        for syl in word_syls:
+            syllables.append([syl.replace("-", ""), False])
+
+    repeated_vowels = [v + v for v in vowels_list] 
+    long_vowels = repeated_vowels + dipthongs
     
-def ATR_val(vowels):
-    pATR = False
-    nATR = False
-
-    for vowel in vowels:
-        if vowel in pATR_list:
-            pATR = True
-        if vowel in nATR_list:
-            nATR = True
     
-    if pATR and nATR:
-        return "Nonharmonic"
-    if pATR:
-        return True
-    if nATR:
-        return False
-    return "error"
+    
+    def syllable_weight(template):
+        # return any(sub_s.endswith(tuple(consonants_list)) or sub_s.endswith(tuple(long_vowels)) for sub_s in s.split('.')) # wrong results in some entries, like "dã́ã́" (98)
+        # also dubious of labeling "C" as heavy (if there are real C-only syllables, C is def the nucleus)
+        return "VC" in template or "VV" in template
 
-def syl_length(words):
-    max_syl = 0
-    max_word = ""
-    for word in words:
-        syllables = word.split('.')
-        for syllable in syllables:
-            if len(syllable) > max_syl:
-                max_syl = len(syllable)
-                max_word = word
-    return max_syl, max_word
+    syllabary = pd.DataFrame(syllables, columns=["Syllable", "Initial"])
+    
+    syllabary['Type Frequency'] = syllabary.groupby(["Syllable", "Initial"]).transform('size')
 
-# this function currently tracks length including unicode characters, so a good idea is to keep track of the top-n (for say n=30) longest syllables and manually check the longest syllable length from there.
+    syllabary = syllabary.drop_duplicates(keep="last")
+
+    syllabary['Vowel'] = syllabary['Syllable'].apply(vowelize)
+
+    syllabary["Template"] = syllabary["Syllable"].apply(templatize)
+
+    syllabary['Syllable Weight'] = syllabary['Template'].apply(syllable_weight)
+    
+    return syllabary
+
+def print_templates(syllabary):
+    template_entries = syllabary["Template"].astype(str)
+    count_entries = syllabary["Type Frequency"].astype(int)
+
+    templates = {}
+
+    for template, count in zip(template_entries, count_entries):
+        if template not in templates:
+            templates[template] = count
+        else:
+            templates[template] += count
+
+    print(templates)
+
 
 df = pd.read_excel("DagaareDict.xlsx")
+syllabary = make_syllabary(df, column = "PH + Syl")
+syllabary.sort_values(by = 'Type Frequency', ascending = False, inplace = True, kind = 'quicksort')
 
-df["PH"] = df["PH"].astype(str)
+print_templates(syllabary)
 
-df["Excluded"] = df["PH"].apply(is_exclude)
-
-df["PH + Syl"] = df["PH"].apply(syllabify)
-
-df["Syl Count"] = df["PH + Syl"].apply(count_syl)
-
-df["Vowels"] = df["PH"].apply(vowelize)
-
-df["Vowels + Syl"] = df["PH + Syl"].apply(vowelize)
-
-df["ATR"] = df["Vowels"].apply(ATR_val)
-
-df["Tones"] = df["PH + Syl"].apply(tonalize, do_syllabify=False)
-
-df["Tones + Syl"] = df["PH + Syl"].apply(tonalize, do_syllabify=True)
-
-df["Nasals"] = df["PH"].apply(nasalize)
-
-df["Nasals + Syl"] = df["PH + Syl"].apply(nasalize)
-
-# df["Nasal + Syl"], df["Tone + Syl"] = zip(*df["PH + Syl"].apply(syllabify_nasal_and_tone, do_syllabify=True))
-
-# df["Nasal"], df["Tone"] = zip(*df["PH"].apply(syllabify_nasal_and_tone, do_syllabify=False))
-
-df.to_excel("DagaareDict.xlsx", index = False)
-
-words = df["PH + Syl"].tolist()
-
-print(syl_length(words))
+syllabary.to_excel("DagaareSyllabary.xlsx", index=False)

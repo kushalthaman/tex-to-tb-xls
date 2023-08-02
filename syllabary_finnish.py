1e602eaf130adf1bab53c3d88f8baebcccb4a2ec
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-08-02 10:45:29 -0700

    reformatting syllabary_finnish.py

--- syllabary_finnish.py
+++ syllabary_finnish.py
@@ -8,14 +8,13 @@ df_gold = df[(df['is.gold'] == 1) & (df['frequency'] > 99)]
 gold0_df = df[df['is.gold'] == 0]
 
 null_gold1 = gold0_df['gold1'].isnull().sum()
-
 print(f"Rows with 'is.gold' == 0 and null 'gold1': {null_gold1}")
-high_freq_gold0_df = gold0_df[gold0_df['frequency'] > 99]
 
+high_freq_gold0_df = gold0_df[gold0_df['frequency'] > 99]
 print(f"Rows with 'is.gold' == 0 and frequency > 99: {len(high_freq_gold0_df)}")
+
 gold_na_df = df[df['is.gold'].isnull()]
 non_null_gold1 = gold_na_df['gold1'].notnull().sum()
-
 print(f"Rows with 'is.gold' == NA and non-null 'gold1': {non_null_gold1}")
 
 high_freq_gold_na_df = gold_na_df[gold_na_df['frequency'] >= 100]

57fcc9cf535234f40df9e3b9b44e24c4c043011c
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-08-07 18:58:01 -0700

    Update syllabary_finnish.py

--- syllabary_finnish.py
+++ syllabary_finnish.py
@@ -7,6 +7,7 @@ df = pd.read_csv('aamulehti-1999.csv', sep='\t')
 print(df.head())
 
 df_gold = df[(df['is.gold'] == 1) & (df['frequency'] > 99)]
+
 gold0_df = df[df['is.gold'] == 0]
 
 null_gold1 = gold0_df['gold1'].isnull().sum()
@@ -25,4 +26,15 @@ print(f"Rows with 'is.gold' == NA and frequency >= 100: {len(high_freq_gold_na_d
 df_gold = df[(df['is-gold'] == 1) & (df['freq'] > 99)]
 print(f"Rows with 'is-gold' == 1 and freq > 99: {len(df_gold)}")
 
-## Making the syllabary
+## syllabary 
+
+syllabary1 = set()
+for col in ["gold1", "gold2", "gold3"]: 
+    syllabary1.update(filtered_df[col].dropna().tolist())
+
+syllabary2 = set()
+for col in ["lem-P1", "lem-P2", "lem-P3"]:  
+    syllabary2.update(filtered_df[col].str.replace("'", "").dropna().tolist()) # remove accent marks? 
+
+print("Syllabary 1:", syllabary1)
+print("Syllabary 2:", syllabary2)

dee36230585ba9fb1fd8fe237c0661d3d778a561
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-08-16 14:43:23 -0700

    Create optimality_theory.py

---
+++ optimality_theory.py
@@ -0,0 +1,9 @@
+# Plot of text frequencies of syllables with simple v/s complex margins
+
+# Onsets and codas in syllables with simple margins (plotted with log frequency)
+
+# Linear, binomial and poisson regression 
+
+# Likelihood ratio testing
+
+# Correlation with zipfian distribution

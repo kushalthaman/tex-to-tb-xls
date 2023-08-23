8660fecd926d220b6aaf528f853dfad3b7c9cc2f
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-08-23 14:03:01 -0700

    Update frequency_analysis.py

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -6,8 +6,8 @@ df = pd.read_excel('FinnishTemplateTable1.xlsx', engine='openpyxl')
 simple_templates = ["VVC", "VV", "VC", "V", "CVVC", "CVV", "CVC", "CV"]
 complex_templates = ["CCCVCC", "CCCVVC", "VCCC", "CCCV", "VVCC", "CVVCC", "CVCCC", "CCCVC", "CCVVC", "CCVV", "CCVCC", "VCC", "CCVC", "CCV", "CVCC", "CCCVCC", "CCCVVC", "VCCC"]
 
-simple_df = df[df['Template'].isin(simple_templates)].groupby('Template').sum()['Token Frequency'].sort_values(ascending=False)
-complex_df = df[df['Template'].isin(complex_templates)].groupby('Template').sum()['Token Frequency'].sort_values(ascending=False)
+simple_df = df[df['Template'].isin(simple_templates)].groupby('Template').sum()['Type Frequency'].sort_values(ascending=False)
+complex_df = df[df['Template'].isin(complex_templates)].groupby('Template').sum()['Type Frequency'].sort_values(ascending=False)
 
 plt.figure(figsize=(10, 6))
 simple_df.plot(kind='barh')

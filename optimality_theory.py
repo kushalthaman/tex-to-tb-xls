2afc1b8f8b0f3de9e90bba8375f061aca88cb436
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-08-22 23:10:52 -0700

    Update optimality_theory.py

--- optimality_theory.py
+++ optimality_theory.py
@@ -2,21 +2,38 @@
 
 # Onsets and codas in syllables with simple margins (plotted with log frequency)
 
+
+
+# Linear, binomial and poisson regression 
+
+# Likelihood ratio testing
+
+# Correlation with zipfian distribution
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_excel('FinnishTemplateTable1.xlsx', engine='openpyxl')
-df = df.sort_values(by='Token Frequency', ascending=False)
+# Load the data from the Excel file
+df = pd.read_excel('data.xlsx', engine='openpyxl')
+
+# Sort by Type Frequency in descending order
+df = df.sort_values(by='Type Frequency', ascending=False)
+
+# Create ranks
 df['Rank'] = range(1, len(df) + 1)
+
+# Log transform
 log_ranks = np.log(df['Rank'])
-log_frequencies = np.log(df['Token Frequency'])
+log_frequencies = np.log(df['Type Frequency'])
+
+# Linear regression
 slope, intercept = np.polyfit(log_ranks, log_frequencies, 1)
 
+# Plotting
 plt.figure(figsize=(10, 6))
 plt.scatter(log_ranks, log_frequencies, color='blue', label="Data")
 plt.plot(log_ranks, slope * log_ranks + intercept, color='red', label=f"Fit: y = {slope:.2f}x + {intercept:.2f}")
-plt.title('Zipf Law Check')
+plt.title('Zipff Law Check')
 plt.xlabel('Log Rank')
 plt.ylabel('Log Frequency')
 plt.legend()
@@ -29,9 +46,3 @@ if -1.1 < slope < -0.9:
     print("The data follows Zipf's law.")
 else:
     print("The data does not follow Zipf's law closely.")
-
-# Linear, binomial and poisson regression 
-
-# Likelihood ratio testing
-
-# Correlation with zipfian distribution

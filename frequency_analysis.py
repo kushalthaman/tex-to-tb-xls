ef6ddb8e2fe1d9ed314fbf6da220dc374129fb22
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-08-23 14:00:23 -0700

    Update frequency_analysis.py

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_excel('FinnishSyllabary1.xlsx', engine='openpyxl')
+df = pd.read_excel('FinnishTemplateTable1.xlsx', engine='openpyxl')
 
 simple_templates = ["VVC", "VV", "VC", "V", "CVVC", "CVV", "CVC", "CV"]
 complex_templates = ["CCCVCC", "CCCVVC", "VCCC", "CCCV", "VVCC", "CVVCC", "CVCCC", "CCCVC", "CCVVC", "CCVV", "CCVCC", "VCC", "CCVC", "CCV", "CVCC", "CCCVCC", "CCCVVC", "VCCC"]
 
-simple_df = df[df['Template'].isin(simple_templates)].groupby('Template').sum()['Token Frequency']
-complex_df = df[df['Template'].isin(complex_templates)].groupby('Template').sum()['Token Frequency']
+simple_df = df[df['Template'].isin(simple_templates)].groupby('Template').sum()['Token Frequency'].sort_values(ascending=False)
+complex_df = df[df['Template'].isin(complex_templates)].groupby('Template').sum()['Token Frequency'].sort_values(ascending=False)
 
 plt.figure(figsize=(10, 6))
-simple_df.plot(kind='barh', color='blue')
+simple_df.plot(kind='barh')
 plt.ylabel('Simple Templates')
 plt.xlabel('Token Frequency')
 plt.title('Text frequencies of syllables with Simple Margins')
@@ -18,7 +18,7 @@ plt.tight_layout()
 plt.show()
 
 plt.figure(figsize=(10, 6))
-complex_df.plot(kind='barh', color='red')
+complex_df.plot(kind='barh')
 plt.ylabel('Complex Templates')
 plt.xlabel('Token Frequency')
 plt.title('Text frequencies of syllables with Complex Margins')

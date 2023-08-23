81685b5934ae39a1adf6d997b91748b4a00f6a27
Kushal Thaman <62183606+kushalthaman@users.noreply.github.com>
2023-08-23 13:42:03 -0700

    Update frequency_analysis.py

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -10,17 +10,17 @@ simple_df = df[df['Template'].isin(simple_templates)].groupby('Template').sum()[
 complex_df = df[df['Template'].isin(complex_templates)].groupby('Template').sum()['Token Frequency']
 
 plt.figure(figsize=(10, 6))
-simple_df.plot(kind='bar', color='blue')
-plt.xlabel('Simple Templates')
-plt.ylabel('Token Frequency')
+simple_df.plot(kind='barh', color='blue')
+plt.ylabel('Simple Templates')
+plt.xlabel('Token Frequency')
 plt.title('Text frequencies of syllables with Simple Margins')
 plt.tight_layout()
 plt.show()
 
 plt.figure(figsize=(10, 6))
-complex_df.plot(kind='bar', color='red')
-plt.xlabel('Complex Templates')
-plt.ylabel('Token Frequency')
+complex_df.plot(kind='barh', color='red')
+plt.ylabel('Complex Templates')
+plt.xlabel('Token Frequency')
 plt.title('Text frequencies of syllables with Complex Margins')
 plt.tight_layout()
 plt.show()
